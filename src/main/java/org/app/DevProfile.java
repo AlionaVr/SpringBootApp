a76d8df5a64aedf1b251f87a2ad95d91332fe955
AlionaVr <alonavershilo@yandex.by>
2025-05-28 22:26:12 +0300

    Add profile-based configuration with SystemProfile interface and REST controller

---
+++ src/main/java/org/app/DevProfile.java
@@ -0,0 +1,8 @@
+package org.app;
+
+public class DevProfile implements SystemProfile {
+    @Override
+    public String getProfile() {
+        return "Current profile is dev";
+    }
+}
---
+++ src/main/java/org/app/SystemProfile.java
@@ -0,0 +1,5 @@
+package org.app;
+
+public interface SystemProfile {
+    String getProfile();
+}
